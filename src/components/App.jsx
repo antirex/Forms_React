25cf41d5ce352ffc442ae81f455907963bb6e233
antirex <55479324+antirex@users.noreply.github.com>
2020-12-01 12:32:12 +0530

    EventListners + States +CR

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [heading, setHeading] = useState("Hey there!");
+  const [styles, setStyles] = useState(false);
+
+  function handleClick() {
+    setHeading("Submitted :)");
+  }
+  function hoverButton() {
+    setStyles(true);
+  }
+  function unHoverButton() {
+    setStyles(false);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        onMouseOver={hoverButton}
+        onMouseOut={unHoverButton}
+        style={{ backgroundColor: styles && "black" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
