a6ca7aa16fa4a75695d3e9ffaee2233fb124e671
antirex <55479324+antirex@users.noreply.github.com>
2020-12-01 13:21:46 +0530

    Onchange+comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-
+import Heading from "./Heading";
 function App() {
-  const [heading, setHeading] = useState("Hey there!");
+  const [heading, setHeading] = useState("");
   const [styles, setStyles] = useState(false);
-
-  function handleClick() {
-    setHeading("Submitted :)");
+  const [input, setInput] = useState("");
+  function handleChange(event) {
+    setInput(event.target.value);
+  }
+  function handleClick(event) {
+    setHeading(input);
+    setInput("");
+    event.preventDefault();
   }
   function hoverButton() {
     setStyles(true);
@@ -15,16 +20,24 @@ function App() {
   }
   return (
     <div className="container">
-      <h1>{heading}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        onClick={handleClick}
-        onMouseOver={hoverButton}
-        onMouseOut={unHoverButton}
-        style={{ backgroundColor: styles && "black" }}
-      >
-        Submit
-      </button>
+      <Heading
+      heading = {heading}
+      />
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={input}
+        />
+        <button
+          onMouseOver={hoverButton}
+          onMouseOut={unHoverButton}
+          style={{ backgroundColor: styles && "black" }}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+function Heading(props){
+  return(
+    <h1>Hey there {props.heading}!</h1>
+  );
+}
+export default Heading;
